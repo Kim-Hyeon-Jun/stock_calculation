208659c0ce26118c2d0d54dfb00c1d4cb0de3d4f
Kim-Hyeon-Jun <76915090+Kim-Hyeon-Jun@users.noreply.github.com>
2021-01-23 15:10:17 +0900

    mainpage.widget
    메인페이지 위젯 2개 (style 제외)

--- lib/main.dart
+++ lib/main.dart
@@ -14,23 +14,13 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Main(title: 'Flutter Demo Home Page'),
+      home: Main(),
     );
   }
 }
 
 class Main extends StatefulWidget {
   Main({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -38,68 +28,56 @@ class Main extends StatefulWidget {
 }
 
 class _MainState extends State<Main> {
-  int _counter = 0;
+  Widget userAccountName = Container(
+    child: Row(
+      children: [
+        Text('증권계좌명'),
+        //여러개의 증권계좌 중 대표 표시
+        Icon(
+          Icons.arrow_drop_down
+        )
+        // 아이콘 클릭시 다른 증권계좌 선택 가능해야함
+      ],
+    ),
+  );
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+ Widget userStrategy = Container(
+   decoration: BoxDecoration(
+     border: Border.all( width: 2)
+   ),
+   child: Column(
+     children: [
+       Container(
+         child: Row(
+           children: [
+             Icon(Icons.functions),
+             Text('투자전략 n')
+           ],
+         ),
+       ),
+       Container(
+         child: Row(
+           children: [
+             Text('손익금액표시')
+             // 클릭시 자신의 '재무제표'란으로 들어감
+           ],
+         ),
+       )
+     ],
+   ),
+ );
+
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the Main object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+         children: [
+         ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
